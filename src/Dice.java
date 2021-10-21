34c5976ee854d1580ff1029de5701a87013d9e3e
amitorko <amitorkofast@gmail.com>
2021-10-20 23:11:38 -0400

    Dice comments

--- src/Dice.java
+++ src/Dice.java
@@ -1,3 +1,6 @@
+/**
+ * Roll and return value of the two dice
+ */
 public class Dice {
     private int dice1;
     private int dice2;
@@ -12,11 +15,30 @@ public class Dice {
         return dice1 + dice2;
     }
 
-    public int currentRoll(){
+    /**
+     * @return the value of the dice
+     */
+    public int rollValue(){
         return dice1 + dice2;
     }
 
+    /**
+     * Check if the dice rolls doubles
+     */
     public boolean isDouble(){
-        return dice1 == dice2;
+        if(dice1 == dice2) {
+            return true;
+        }
+        return false;
     }
+    /**
+     * Check if the dice rolls triples
+     */
+    public boolean isTriple(){
+        if(dice1 == dice2 && isDouble()) {
+            return true;
+        }
+        return false;
+    }
+
 }
