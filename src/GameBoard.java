401cd4eca31c6df91616df51385ef94a13969791
MohammadGaffori <mohammadsen@gmail.com>
2021-10-22 10:15:25 -0400

    CreateBoard() complete for this deliverable

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -6,40 +6,51 @@ public class GameBoard {
 
     private Square[] properties;
 
+    private String teal = "teal";
+    private String brown = "brown";
+    private String pink = "pink";
+    private String orange = "orange";
+    private String blue = "blue";
+    private String red = "red";
+    private String yellow = "yellow";
+    private String green = "green";
+
+
+
     private void createBoard() {
-        properties = new Square[19];
+        properties = new Square[21];
 
-        properties[0] = new Property("Go", 0, 0);
+        properties[0] = new Property("Go", 0, 0, "No Color");
 
-        properties[1] = new Property("Mediterranean Avenue", 60,2);
-        properties[2] = new Property("Baltic Avenue", 60,4);
+        properties[1] = new Property("Mediterranean Avenue", 60,2, brown);
+        properties[2] = new Property("Baltic Avenue", 60,4, brown);
 
-        properties[3] = new Property("Oriental Avenue", 100);
-        properties[4] = new Property("Vermont Avenue", 100);
-        properties[5] = new Property("Connecticut Avenue", 120);
+        properties[3] = new Property("Oriental Avenue", 100,6, teal);
+        properties[4] = new Property("Vermont Avenue", 100, 6, teal);
+        properties[5] = new Property("Connecticut Avenue", 120, 8, teal);
 
-        properties[6] = new Property("St. Charles Place", 140);
-        properties[7] = new Property("States Avenue", 140);
-        properties[8] = new Property("Virginia Avenue", 140);
+        properties[6] = new Property("St. Charles Place", 140, 10, pink);
+        properties[7] = new Property("States Avenue", 140, 10, pink );
+        properties[8] = new Property("Virginia Avenue", 160, 12, pink);
 
-        properties[9] = new Property("St. James Place", 140);
-        properties[10] = new Property("Tennessee Avenue", 140);
-        properties[11] = new Property("New York Avenue", 140);
+        properties[9] = new Property("St. James Place", 180,14, orange);
+        properties[10] = new Property("Tennessee Avenue", 180, 14, orange);
+        properties[11] = new Property("New York Avenue", 200,16, orange);
 
-        properties[12] = new Property("Kentucky Avenue", 140);
-        properties[13] = new Property("Indiana Avenue", 140);
-        properties[14] = new Property("Illinois Avenue", 140);
+        properties[12] = new Property("Kentucky Avenue", 220,18, red);
+        properties[13] = new Property("Indiana Avenue", 220, 18, red);
+        properties[14] = new Property("Illinois Avenue", 240,20, red);
 
-        properties[15] = new Property("Atlantic Avenue", 140);
-        properties[16] = new Property("Ventnor Avenue", 140);
-        properties[17] = new Property("Marvin Gardens", 140);
+        properties[15] = new Property("Atlantic Avenue", 260,22, yellow);
+        properties[16] = new Property("Ventnor Avenue", 260, 22, yellow);
+        properties[17] = new Property("Marvin Gardens", 280, 24, yellow);
 
-        properties[18] = new Property("Atlantic Avenue", 140);
-        properties[19] = new Property("Ventnor Avenue", 140);
-        properties[20] = new Property("Marvin Gardens", 140);
+        properties[18] = new Property("Atlantic Avenue", 300,26, green);
+        properties[19] = new Property("Ventnor Avenue", 300,26, green);
+        properties[20] = new Property("Marvin Gardens", 320, 28, green);
 
-        properties[21] = new Property("Atlantic Avenue", 140);
-        properties[22] = new Property("Ventnor Avenue", 140);
+        properties[21] = new Property("Atlantic Avenue", 350, 35, blue);
+        properties[22] = new Property("Ventnor Avenue", 400, 50, blue );
 
 
     }
