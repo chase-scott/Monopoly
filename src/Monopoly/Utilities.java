043c0ccc91fef741c7df399fea09bfe34d3c98b5
amitorko <amitorkofast@gmail.com>
2021-11-19 19:41:40 -0500

    utilities rough 1

--- src/Monopoly/Utilities.java
+++ src/Monopoly/Utilities.java
@@ -8,12 +8,39 @@ public class Utilities extends Square{
      *
      * @param name String, the name of the square
      */
-    public Utilities(String name) {
+    private final int  price = 150;
+    private static Dice dice = new Dice();
+    private int amountOwed;
+    GameBoard gameBoard= new GameBoard();
+    private Player ownedBy = null;
+    public Utilities(String name, Dice dice,GameBoard gameBoard) {
         super(name);
+        this.gameBoard=gameBoard;
+        this.dice=dice;
+    }
+
+    public Player getOwner() {
+        return ownedBy;
+    }
+
+    public void setOwner(Player currentPlayer){
+        ownedBy = currentPlayer;
+        //double playerMoney = currentPlayer.getMoney();
+        //currentPlayer.setMoney(playerMoney-200);
     }
 
     @Override
     public void squareAction(Player player) {
+        if(ownedBy != null && ownedBy != player) {
+            int playerRoll = dice.roll();
+            Square u1 = gameBoard.getSquare(12);
+            Square u2 = gameBoard.getSquare(22);
+            if(((Utilities)u1).getOwner()==ownedBy && ((Utilities)u2).getOwner()==ownedBy ){
+                amountOwed=10*playerRoll;
+            }else{
+                amountOwed=4*playerRoll;
+            }
+        }
 
     }
 
